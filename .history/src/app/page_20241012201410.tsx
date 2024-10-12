8a4c33743a0f3d0ea6f6aaf63a8aff4f4424b412
Rohit Jangid <73385164+rohitjangid666@users.noreply.github.com>
2024-10-12 20:14:59 +0530

    initial commit

---
+++ .history/src/app/page_20241012201410.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      
+    </div>
+  );
+}
--- .history/src/app/page_20241012201422.tsx
+++ .history/src/app/page_20241012201422.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className='min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      HOME
+      H
     </div>
   );
 }
